9a8fdcc31afa9ba1d5166eea3aec6c37b7fe45e2
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-01-20 12:38:58 +0700

    Update KeyApiClient.m

--- API/KeyApiClient.m
+++ API/KeyApiClient.m
@@ -1,91 +1,87 @@
-#import "APIClient.h"
+#import "KeyAPIClient.h"
 
-@interface APIClient ()
+@implementation KeyAPIClient
 
-@property (nonatomic, strong) AFHTTPSessionManager *sessionManager;
+@synthesize apiBase;
+@synthesize accessToken;
 
-@end
-
-@implementation KeyApiClient
-
-+ (APIClient *)sharedClient {
-  static APIClient *sharedClient = nil;
-  static dispatch_once_t onceToken;
-  dispatch_once(&onceToken, ^{
-    sharedClient = [[APIClient alloc] init];
-  });
-  return sharedClient;
+- (void)setAccessToken:(NSString *)accessToken {
+    self.accessToken = accessToken;
 }
 
-- (instancetype)init {
-  self = [super init];
-  if (self) {
-    _sessionManager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"https://example.com"]];
-    _sessionManager.responseSerializer = [AFJSONResponseSerializer serializer];
-  }
-  return self;
-}
+- (KeyAPIClientResponseCode)apiRequest:(NSString *)apiPath params:(NSDictionary *)params {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/%@", self.apiBase, apiPath]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[[params JSONString] dataUsingEncoding:NSUTF8StringEncoding]];
 
-- (void)setApiAccessToken:(NSString *)apiAccessToken {
-  _apiAccessToken = apiAccessToken;
-}
+    NSError *error;
+    NSHTTPURLResponse *response;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
 
-- (APIClientStatus)apiRequest:(NSDictionary *)params {
-  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[self.apiBase stringByAppendingPathComponent:@"api/v1"]]];
-  request.HTTPMethod = @"POST";
-  request.HTTPBody = [NSJSONSerialization dataWithJSONObject:params options:0 error:nil];
-  request.allHTTPHeaderFields = @{@"Authorization": [NSString stringWithFormat:@"Bearer %@", self.apiAccessToken]};
-
-  NSURLSessionDataTask *task = [self.sessionManager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
     if (error) {
-      return;
+        return KeyAPIClientResponseCodeFail;
     }
 
-    NSDictionary *json = responseObject;
-    APIClientStatus status = APIClientStatusFail;
-    if ([json[@"status"] isEqualToString:@"success"]) {
-      status = APIClientStatusSuccess;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+
+    if (error) {
+        return KeyAPIClientResponseCodeFail;
     }
 
-    if (self.onSuccess) {
-      self.onSuccess();
+    NSString *status = json[@"status"];
+
+    if ([status isEqualToString:@"success"]) {
+        return KeyAPIClientResponseCodeSuccess;
+    } else {
+        return KeyAPIClientResponseCodeFail;
     }
-  }];
-
-  [task resume];
-
-  return status;
 }
 
-- (APIClientStatus)checkUdid {
-  NSDictionary *params = @{@"device_test": @"test"};
-  return [self apiRequest:params];
+- (KeyAPIClientResponseCode)checkUdid {
+    NSDictionary *params = @{@"device_test" : @"test"};
+
+    KeyAPIClientResponseCode code = [self apiRequest:@"/check_udid" params:params];
+
+    if (code == KeyAPIClientResponseCodeSuccess) {
+        return code;
+    } else {
+        return [self requestUdid];
+    }
 }
 
-- (APIClientStatus)requestUdid {
-  NSDictionary *params = @{@"device_test": @"test"};
-  APIClientStatus status = [self apiRequest:params];
-  if (status == APIClientStatusSuccess) {
-    self.apiBase = json[@"mobile_config_url"];
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:self.apiBase]];
-  }
-  return status;
+- (KeyAPIClientResponseCode)requestUdid {
+    NSDictionary *params = @{@"device_test" : @"test"};
+
+    KeyAPIClientResponseCode code = [self apiRequest:@"/request_udid" params:params];
+
+    if (code == KeyAPIClientResponseCodeSuccess) {
+        NSString *mobile_config_url = json[@"mobile_config_url"];
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:mobile_config_url]];
+        return code;
+    } else {
+        return code;
+    }
 }
 
-- (APIClientStatus)checkKey:(NSString *)key value:(NSString *)value {
-  NSDictionary *params = @{@"key": key, @"value": value};
-  return [self apiRequest:params];
+- (KeyAPIClientResponseCode)checkKey:(NSString *)key {
+    NSDictionary *params = @{@"key" : key};
+
+    KeyAPIClientResponseCode code = [self apiRequest:@"/check_key" params:params];
+
+    if (code == KeyAPIClientResponseCodeSuccess) {
+        return code;
+    } else {
+        return code;
+    }
 }
 
-- (void)onSuccess:(void (^)(void))onsuccess {
-  self.onSuccess = onsuccess;
+- (void)onSuccess:(void (^)(void))completeBlock {
+    self.onSuccessBlock = completeBlock;
 }
 
 - (void)execute {
-  APIClientStatus status = [self checkUdid];
-  if (status == APIClientStatusSuccess) {
-    [self checkKey:@"key" value:@"value"];
-  }
+    [self checkUdid];
 }
 
 @end
