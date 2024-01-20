5339fa2902f9a3da9fd6b85a45443a43204d1a60
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-01-20 13:16:05 +0700

    Update KeyApiClient.h

--- API/KeyApiClient.h
+++ API/KeyApiClient.h
@@ -1,10 +1,5 @@
 #import <Foundation/Foundation.h>
 
-typedef NS_ENUM(NSInteger, KeyAPIClientResponseCode) {
-    KeyAPIClientResponseCodeSuccess = 0,
-    KeyAPIClientResponseCodeFail = 1,
-};
-
 @interface KeyAPIClient : NSObject
 
 @property (nonatomic, strong) NSString *apiBase;
@@ -13,12 +8,6 @@ typedef NS_ENUM(NSInteger, KeyAPIClientResponseCode) {
 - (void)setAccessToken:(NSString *)accessToken;
 
 - (KeyAPIClientResponseCode)apiRequest:(NSString *)apiPath params:(NSDictionary *)params;
-
-- (KeyAPIClientResponseCode)checkUdid;
-
-- (KeyAPIClientResponseCode)requestUdid;
-
-- (KeyAPIClientResponseCode)checkKey:(NSString *)key;
 
 - (void)onSuccess:(void (^)(void))completeBlock;
 
