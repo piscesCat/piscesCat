5212b008db4ee628fab8822c428c95f4cf30181f
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-07-06 22:16:01 +0700

    Update zte_modem_reboot.sh

--- zte_modem_reboot.sh
+++ zte_modem_reboot.sh
@@ -7,15 +7,24 @@ LOGIN_RESPONSE=$(curl -s --header "Referer: http://$MODEM_IP/index.html" -d "isT
 
 if [[ $LOGIN_RESPONSE == *'"result":"0"'* ]]; then
     echo "Login OK"
-    REBOOT_RESPONSE=$(curl -s "http://$MODEM_IP/goform/goform_set_cmd_process" \
-    -X POST \
-    -H "Referer: http://$MODEM_IP/index.html" \
-    -d "isTest=false&goformId=REBOOT_DEVICE")
     
-    if [[ $REBOOT_RESPONSE == *'"result":"success"'* ]]; then
-        echo "Reboot OK"
+    NETWORK_PROVIDER_RESPONSE=$(curl -s -H "Referer: http://$MODEM_IP/index.html" "http://$MODEM_IP/goform/goform_get_cmd_process?isTest=false&cmd=network_provider")
+    
+    if [[ $NETWORK_PROVIDER_RESPONSE != *'"network_provider":""'* ]]; then
+        echo "Network provider is not empty. Rebooting..."
+        
+        REBOOT_RESPONSE=$(curl -s "http://$MODEM_IP/goform/goform_set_cmd_process" \
+        -X POST \
+        -H "Referer: http://$MODEM_IP/index.html" \
+        -d "isTest=false&goformId=REBOOT_DEVICE")
+        
+        if [[ $REBOOT_RESPONSE == *'"result":"success"'* ]]; then
+            echo "Reboot OK"
+        else
+            echo "Reboot FAILED"
+        fi
     else
-        echo "Reboot FAILED"
+        echo "Network provider is empty. No reboot needed."
     fi
 else
     echo "Login FAILED"
