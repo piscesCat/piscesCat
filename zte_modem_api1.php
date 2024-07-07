874e4407c86c40a7c0c28e6497e3023104ee4c16
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-07-07 21:16:25 +0700

    Update zte_modem_api2.php

--- zte_modem_api1.php
+++ zte_modem_api1.php
@@ -60,11 +60,60 @@ function fetchSmsListFromModem($modem_ip) {
     return $decoded_response;
 }
 
+function sendSmsFromModem($modem_ip, $phone_number, $message) {
+    $url = "http://$modem_ip/goform/goform_set_cmd_process";
+    $headers = ["Referer: http://$modem_ip/index.html"];
+    
+    $encoded_message = encode_to_hex($message);
+    $encoded_phone_number = urlencode($phone_number);
+    $current_datetime = date("d;m;y;H;i;s;O");
+    $encoded_date = urlencode($current_datetime);
+    
+    $payload = [
+        "isTest" => "false",
+        "goformId" => "SEND_SMS",
+        "notCallback" => "true",
+        "Number" => $encoded_phone_number,
+        "sms_time" => $encoded_date,
+        "MessageBody" => $encoded_message,
+        "ID" => "-1",
+        "encode_type" => "GSM7_default"
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if ($response) {
+        $result = json_decode($response, true);
+        if (isset($result["result"]) && $result["result"] == "success") {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 $modem_ip = "192.168.0.1";
 $password = "admin";
 
 if (loginToModem($modem_ip, $password)) {
     echo "Login OK\n";
+
+    $phone_number = "1414";
+    $message = "ACTIVE NETWORK";
+    $send_result = sendSmsFromModem($modem_ip, $phone_number, $message);
+    if ($send_result) {
+        echo "SMS sent.\n";
+    } else {
+        echo "Send SMS failed.\n";
+    }
 
     $sms_list = fetchSmsListFromModem($modem_ip);
     echo "SMS List from modem: \n";
@@ -72,4 +121,3 @@ if (loginToModem($modem_ip, $password)) {
 } else {
     echo "Login Failed";
 }
-?>
