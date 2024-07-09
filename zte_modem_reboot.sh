b13856fd42164c1df6b60e63c5a938d364afe913
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-07-10 00:07:40 +0700

    Update zte_modem_reboot.sh

--- zte_modem_reboot.sh
+++ zte_modem_reboot.sh
@@ -1,33 +1,92 @@
-#!/bin/bash
+#!/usr/bin/env php-cli
+<?php
 
-MODEM_IP="192.168.0.1"
-PASSWORD="YWRtaW4%3D"
+$MODEM_IP = "192.168.0.1";
+$PASSWORD = "admin";
 
-LOGIN_RESPONSE=$(curl -s --header "Referer: http://$MODEM_IP/index.html" -d "isTest=false&goformId=LOGIN&password=$PASSWORD" http://$MODEM_IP/goform/goform_set_cmd_process)
+$encoded_password = base64_encode($PASSWORD);
 
-if [[ $LOGIN_RESPONSE == *'"result":"0"'* ]]; then
-    RESPONSE=$(curl -s -H "Referer: http://$MODEM_IP/index.html" "http://$MODEM_IP/goform/goform_get_cmd_process?isTest=false&cmd=Device_Connected_Time,msisdn")
-    
-    DEVICE_CONNECTED_TIME=$(echo $RESPONSE | grep -o '"Device_Connected_Time":"[^"]*"' | cut -d':' -f2 | tr -d '"')
-    MSISDN=$(echo $RESPONSE | grep -o '"msisdn":"[^"]*"' | cut -d':' -f2 | tr -d '"')
+$login_data = array(
+    'isTest' => 'false',
+    'goformId' => 'LOGIN',
+    'password' => $encoded_password
+);
 
-    echo "Device_Connected_Time: $DEVICE_CONNECTED_TIME"
-    echo "MSISDN: $MSISDN"
+$login_response = curl_post("http://$MODEM_IP/goform/goform_set_cmd_process", $login_data);
 
-    if [[ $DEVICE_CONNECTED_TIME -ge 120 && $MSISDN != "" ]]; then
-        REBOOT_RESPONSE=$(curl -s "http://$MODEM_IP/goform/goform_set_cmd_process" \
-        -X POST \
-        -H "Referer: http://$MODEM_IP/index.html" \
-        -d "isTest=false&goformId=REBOOT_DEVICE")
-        
-        if [[ $REBOOT_RESPONSE == *'"result":"success"'* ]]; then
-            echo "Reboot OK"
-        else
-            echo "Reboot FAILED"
-        fi
-    else
-        echo "No reboot needed."
-    fi
-else
-    echo "Login FAILED"
-fi
+if ($login_response !== false) {
+    $login_result = json_decode($login_response, true);
+
+    if ($login_result && isset($login_result['result']) && $login_result['result'] === '0') {
+        $cmd_data = array(
+            'isTest' => 'false',
+            'cmd' => 'Device_Connected_Time,msisdn'
+        );
+
+        $response = curl_get("http://$MODEM_IP/goform/goform_get_cmd_process", $cmd_data);
+
+        if ($response !== false) {
+            $response_data = json_decode($response, true);
+
+            if ($response_data && isset($response_data['Device_Connected_Time'], $response_data['msisdn'])) {
+                $device_connected_time = (int)$response_data['Device_Connected_Time'];
+                $msisdn = $response_data['msisdn'];
+
+                echo "Device_Connected_Time: $device_connected_time\n";
+                echo "MSISDN: $msisdn\n";
+
+                if ($device_connected_time >= 120 && $msisdn !== "") {
+                    $reboot_data = array(
+                        'isTest' => 'false',
+                        'goformId' => 'REBOOT_DEVICE'
+                    );
+
+                    $reboot_response = curl_post("http://$MODEM_IP/goform/goform_set_cmd_process", $reboot_data);
+
+                    if ($reboot_response !== false) {
+                        $reboot_result = json_decode($reboot_response, true);
+
+                        if ($reboot_result && isset($reboot_result['result']) && $reboot_result['result'] === 'success') {
+                            echo "Rebooting...\n";
+                        } else {
+                            echo "Reboot FAILED\n";
+                        }
+                    } else {
+                        echo "Reboot request failed\n";
+                    }
+                } else {
+                    echo "No reboot needed.\n";
+                }
+            } else {
+                echo "Invalid or missing data in response\n";
+            }
+        } else {
+            echo "Failed to retrieve response\n";
+        }
+    } else {
+        echo "Login FAILED\n";
+    }
+} else {
+    echo "Login request failed\n";
+}
+
+function curl_post($url, $data) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+function curl_get($url, $data) {
+    $url = $url . '?' . http_build_query($data);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
