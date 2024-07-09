17fa8b1d4ea845f422cbf28d543a0d6f1cef6843
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-07-09 23:54:13 +0700

    Update zte_modem_reboot.sh

--- zte_modem_reboot.sh
+++ zte_modem_reboot.sh
@@ -6,23 +6,22 @@ PASSWORD="YWRtaW4%3D"
 LOGIN_RESPONSE=$(curl -s --header "Referer: http://$MODEM_IP/index.html" -d "isTest=false&goformId=LOGIN&password=$PASSWORD" http://$MODEM_IP/goform/goform_set_cmd_process)
 
 if [[ $LOGIN_RESPONSE == *'"result":"0"'* ]]; then
-    echo "Login OK"
-    
     RESPONSE=$(curl -s -H "Referer: http://$MODEM_IP/index.html" "http://$MODEM_IP/goform/goform_get_cmd_process?isTest=false&cmd=Device_Connected_Time,msisdn")
     
     DEVICE_CONNECTED_TIME=$(echo $RESPONSE | grep -o '"Device_Connected_Time":"[0-9]*"' | grep -o '[0-9]*')
     MSISDN=$(echo $RESPONSE | grep -o '"msisdn":"[^"]*"' | cut -d':' -f2 | tr -d '"')
 
+    echo "Device_Connected_Time: $DEVICE_CONNECTED_TIME"
+    echo "MSISDN: $MSISDN"
+
     if [[ $DEVICE_CONNECTED_TIME -ge 120 && $MSISDN != "" ]]; then
-        echo "Rebooting..."
-        
         REBOOT_RESPONSE=$(curl -s "http://$MODEM_IP/goform/goform_set_cmd_process" \
         -X POST \
         -H "Referer: http://$MODEM_IP/index.html" \
         -d "isTest=false&goformId=REBOOT_DEVICE")
         
         if [[ $REBOOT_RESPONSE == *'"result":"success"'* ]]; then
-            echo "Reboot OK"
+            echo "Rebooting..."
         else
             echo "Reboot FAILED"
         fi
